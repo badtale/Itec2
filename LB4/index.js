a28e1209850242b38270e26c38a31321a33fb877
Yura Petrychenko <urapetricenko860@gmail.com>
2022-05-29 12:06:40 +0300

    Made 4th lab

---
+++ LB4/index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	  var clientIp = socket.request.connection.remoteAddress;
+	io.emit('chat message', 'New connection from ' + clientIp);
+  socket.on('chat message', msg => {
+	console.log(msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
